43aa7c6e9d7e2995130c2a20460f9c86b5c444fd
Alexandre Bernardini <bernardini.alexandre2003@gmail.com>
2024-10-15 10:05:15 +0100

    GrapQL avec Netflix

--- src/components/graphql.ts
+++ src/components/graphql.ts
@@ -6,151 +6,273 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 // Interfaces TypeScript
-interface CreateBookInput {
+interface CreateFilmInput {
     title: string;
-    authorId: number;
-    categoryId: number;
+    directorId: number;
+    genreId: number;
+    releaseYear: number;
 }
 
-interface UpdateBookInput {
-    title: string;
-    authorId: number;
-    categoryId: number;
+interface UpdateFilmInput {
+    title?: string;
+    directorId?: number;
+    genreId?: number;
+    releaseYear?: number;
+}
+
+interface CreateDirectorInput {
+    name: string;
+}
+
+interface CreateGenreInput {
+    name: string;
+}
+
+interface CreateUserInput {
+    username: string;
 }
 
 // Définition du schéma GraphQL
 const typeDefs = `#graphql
-    type Book {
+    type Film {
         id: Int!
         title: String!
-        author: Author!
-        category: Category!
+        director: Director!
+        genre: Genre!
+        releaseYear: Int!
+        ratings: [Rating!]!
     }
 
-    type Author {
+    type Director {
         id: Int!
         name: String!
-        books: [Book!]!
+        films: [Film!]!
     }
 
-    type Category {
+    type Genre {
         id: Int!
         name: String!
-        books: [Book!]!
+        films: [Film!]!
+    }
+
+    type User {
+        id: Int!
+        username: String!
+        ratings: [Rating!]!
+    }
+
+    type Rating {
+        id: Int!
+        score: Int!
+        user: User!
+        film: Film!
     }
 
     type Query {
-        books: [Book!]!
-        bookById(id: Int!): Book
-        categories: [Category!]!
-        authors: [Author!]!
+        films: [Film!]!
+        filmById(id: Int!): Film
+        directors: [Director!]!
+        genres: [Genre!]!
+        users: [User!]!
     }
 
-    input CreateBookInput {
+    input CreateFilmInput {
         title: String!
-        authorId: Int!
-        categoryId: Int!
+        directorId: Int!
+        genreId: Int!
+        releaseYear: Int!
     }
-    
-    input UpdateBookInput {
-        title: String!
-        authorId: Int!
-        categoryId: Int!
+
+    input UpdateFilmInput {
+        title: String
+        directorId: Int
+        genreId: Int
+        releaseYear: Int
+    }
+
+    input CreateDirectorInput {
+        name: String!
+    }
+
+    input CreateGenreInput {
+        name: String!
+    }
+
+    input CreateUserInput {
+        username: String!
+    }
+
+    input CreateRatingInput {
+        score: Int!
+        userId: Int!
+        filmId: Int!
     }
 
     type Mutation {
-        createBook(input: CreateBookInput!): Book!
-        updateBook(id: Int!, input: UpdateBookInput!): Book!
+        createFilm(input: CreateFilmInput!): Film!
+        updateFilm(id: Int!, input: UpdateFilmInput!): Film!
+        createDirector(input: CreateDirectorInput!): Director!
+        createGenre(input: CreateGenreInput!): Genre!
+        createUser(input: CreateUserInput!): User!  # Ajout de la mutation pour créer un utilisateur
+        createRating(input: CreateRatingInput!): Rating!
     }
 `;
 
 // Résolveurs avec Prisma
 const resolvers = {
     Query: {
-        books: async () => {
-            return await prisma.book.findMany({
+        films: async () => {
+            return await prisma.film.findMany({
                 include: {
-                    author: true,
-                    category: true,
+                    director: true,
+                    genre: true,
+                    ratings: {
+                        include: {
+                            user: true,
+                        },
+                    },
                 },
             });
         },
-        bookById: async (_: any, { id }: { id: number }) => {
-            return await prisma.book.findUnique({
+        filmById: async (_: any, { id }: { id: number }) => {
+            return await prisma.film.findUnique({
                 where: { id },
                 include: {
-                    author: true,
-                    category: true,
+                    director: true,
+                    genre: true,
+                    ratings: {
+                        include: {
+                            user: true,
+                        },
+                    },
                 },
             });
         },
-        authors: async () => {
-            return await prisma.author.findMany({
-                include: { books: true },
+        directors: async () => {
+            return await prisma.director.findMany({
+                include: { films: true },
             });
         },
-        categories: async () => {
-            return await prisma.category.findMany({
-                include: { books: true },
+        genres: async () => {
+            return await prisma.genre.findMany({
+                include: { films: true },
+            });
+        },
+        users: async () => {
+            return await prisma.user.findMany({
+                include: { ratings: true },
             });
         },
     },
 
     Mutation: {
-        createBook: async (_: any, { input }: { input: CreateBookInput }) => {
-            return await prisma.book.create({
+        createFilm: async (_: any, { input }: { input: CreateFilmInput }) => {
+            return await prisma.film.create({
                 data: {
                     title: input.title,
-                    author: { connect: { id: input.authorId } },
-                    category: { connect: { id: input.categoryId } },
+                    releaseYear: input.releaseYear,
+                    director: { connect: { id: input.directorId } },
+                    genre: { connect: { id: input.genreId } },
                 },
                 include: {
-                    author: true,
-                    category: true,
+                    director: true,
+                    genre: true,
                 },
             });
         },
-        updateBook: async (_: any, { id, input }: { id: number; input: UpdateBookInput }) => {
-            return await prisma.book.update({
+        updateFilm: async (_: any, { id, input }: { id: number; input: UpdateFilmInput }) => {
+            return await prisma.film.update({
                 where: { id },
                 data: {
-                    title: input.title,
-                    author: { connect: { id: input.authorId } },
-                    category: { connect: { id: input.categoryId } },
+                    title: input.title ?? undefined,
+                    releaseYear: input.releaseYear ?? undefined,
+                    director: input.directorId
+                        ? { connect: { id: input.directorId } }
+                        : undefined,
+                    genre: input.genreId ? { connect: { id: input.genreId } } : undefined,
                 },
                 include: {
-                    author: true,
-                    category: true,
+                    director: true,
+                    genre: true,
+                },
+            });
+        },
+        createDirector: async (_: any, { input }: { input: CreateDirectorInput }) => {
+            return await prisma.director.create({
+                data: {
+                    name: input.name,
+                },
+            });
+        },
+        createGenre: async (_: any, { input }: { input: CreateGenreInput }) => {
+            return await prisma.genre.create({
+                data: {
+                    name: input.name,
+                },
+            });
+        },
+        createUser: async (_: any, { input }: { input: CreateUserInput }) => {
+            return await prisma.user.create({
+                data: {
+                    username: input.username,
+                },
+            });
+        },
+        createRating: async (_: any, { input }: { input: { score: number; userId: number; filmId: number } }) => {
+            return await prisma.rating.create({
+                data: {
+                    score: input.score,
+                    user: { connect: { id: input.userId } },
+                    film: { connect: { id: input.filmId } },
+                },
+                include: {
+                    user: true,
+                    film: true,
                 },
             });
         },
     },
 
-    Book: {
-        author: async (parent: any) => {
-            return await prisma.author.findUnique({
-                where: { id: parent.authorId },
+    Film: {
+        director: async (parent: any) => {
+            return await prisma.director.findUnique({
+                where: { id: parent.directorId },
             });
         },
-        category: async (parent: any) => {
-            return await prisma.category.findUnique({
-                where: { id: parent.categoryId },
+        genre: async (parent: any) => {
+            return await prisma.genre.findUnique({
+                where: { id: parent.genreId },
+            });
+        },
+        ratings: async (parent: any) => {
+            return await prisma.rating.findMany({
+                where: { filmId: parent.id },
+                include: { user: true },
             });
         },
     },
 
-    Author: {
-        books: async (parent: any) => {
-            return await prisma.book.findMany({
-                where: { authorId: parent.id },
+    Director: {
+        films: async (parent: any) => {
+            return await prisma.film.findMany({
+                where: { directorId: parent.id },
             });
         },
     },
 
-    Category: {
-        books: async (parent: any) => {
-            return await prisma.book.findMany({
-                where: { categoryId: parent.id },
+    Genre: {
+        films: async (parent: any) => {
+            return await prisma.film.findMany({
+                where: { genreId: parent.id },
+            });
+        },
+    },
+
+    User: {
+        ratings: async (parent: any) => {
+            return await prisma.rating.findMany({
+                where: { userId: parent.id },
+                include: { film: true },
             });
         },
     },
